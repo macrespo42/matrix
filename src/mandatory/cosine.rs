fc3aa9d10bcf2b5f46f18ae8efb56ebcab930706
macrespo42 <macrespo@student.42.fr>
2023-07-17 17:11:03 +0200

    feat: rounded proof for cosine

--- src/mandatory/cosine.rs
+++ src/mandatory/cosine.rs
@@ -74,4 +74,42 @@ mod tests {
         let v = Vector::from(&[6., 4.]);
         assert_eq!(angle_cos(&u, &v), -0.54626775);
     }
+
+    #[test]
+    fn cosine_proof() {
+        let u = Vector::from(&[8., 7.]);
+        let v = Vector::from(&[3., 2.]);
+        println!("test with\n{}\n{}", u, v);
+        println!("{}", angle_cos(&u, &v));
+        assert_eq!(angle_cos(&u, &v), 0.99145424);
+        let norm_u_f32 = u.norm();
+        let norm_calculator_u_f32 = 10.630145812735 as f32;
+        assert_eq!(norm_u_f32, norm_calculator_u_f32);
+        let norm_v_f32 = v.norm();
+        let norm_calculator_v_f32 = 3.605551275464 as f32;
+        assert_eq!(norm_v_f32, norm_calculator_v_f32);
+        let dot_f32 = u.dot(v);
+        let dot_calculator = 38 as f32;
+        assert_eq!(dot_f32, dot_calculator);
+        println!(
+        "norm u : {} | norm from calculator online {}\nnorm v {} | norm v from calculator online {}\ndot {} | dot on calculator {}",
+        norm_u_f32, norm_calculator_u_f32, norm_v_f32, norm_calculator_v_f32,dot_f32, dot_calculator
+    );
+        let dot_norm = norm_u_f32 * norm_v_f32;
+        let dot_norm_calculator = norm_calculator_u_f32 * norm_calculator_v_f32;
+        assert_eq!(dot_norm, dot_norm_calculator);
+        println!(
+            "dot product on norm {:?} | dot product with value of generator {}",
+            (dot_norm),
+            (norm_calculator_u_f32 * norm_calculator_v_f32)
+        );
+        let angle_cos_result = dot_f32 / dot_norm;
+        let angle_cos_calculator = dot_calculator / dot_norm_calculator;
+        assert_eq!(angle_cos_result, angle_cos_calculator);
+        println!(
+            "angle cos result : {} | angle cos result calculator : {}",
+            angle_cos_result, angle_cos_calculator
+        );
+        println!("------------------------------------------------------");
+    }
 }
